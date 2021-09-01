76a725f77e861e78432995b191fe7e1fd62b8694
김은영 <solaz0824@gmail.com>
2021-09-01 12:34:40 +0200

    Get products from server working

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product';
 import useStyles from './styles'
 import Navbar from '../Navbar/Navbar';
 
-const products = [
-    {id: 1,name:'Alhambra',description:'Some information about the beer because people want to know what they drink',price:'€1,00',image:'https://www.loscervecistas.es/wp-content/uploads/2016/09/verde-detalle.png'  },
-    {id: 2,name:'Moritz',description:'Some information about the beer because people want to know what they drink',price:'€1,30',image:'https://moritz.com/media/wysiwyg/MORITZ.png' },
-    {id: 3,name:'Red Horse',description:'Some information about the beer because people want to know what they drink',price:'€2,50',image:'https://www.monde-selection.com/wp-content/uploads/2019/08/_DSF0521.png' },
-    {id: 4,name:'Cusqueña',description:'Some information about the beer because people want to know what they drink',price:'€2,00',image:'https://jotajotafoods.com/wp-content/uploads/2020/07/CER00014.png' },
-    {id: 5,name:'Feldschlösschen',description:'Some information about the beer because people want to know what they drink',price:'€3,00',image:'https://www.carlsberggroup.com/media/1436/fieldschlossen-weihnachtsbier.png' },
-    {id: 6,name:'Perła ',description:'Some information about the beer because people want to know what they drink',price:'€2,60',image:'https://topdeli.co.uk/wp-content/uploads/2018/03/perla-but.png' },   
-    {id: 7,name:'Närke Kaggen Stormaktsporter',description:'Some information about the beer because people want to know what they drink',price:'€3,20',image:'https://glasbanken.se/wp-content/uploads/Kaggen-Stormaktsporter-2019-Na%CC%88rke-Kulturbryggeri.png' },
-    {id: 8,name:'Hite',description:'Some information about the beer because people want to know what they drink',price:'€4,00',image:'https://produits.bienmanger.com/35121-0w470h470_Hite_Lager_Beer_From_Kore.jpg' }
-]
+import axios from "axios";
+
+
+
+// const products = [
+//     {id: 1,name:'Alhambra',description:'Some information about the beer because people want to know what they drink',price:'€1,00',image:'https://www.loscervecistas.es/wp-content/uploads/2016/09/verde-detalle.png'  },
+//     {id: 2,name:'Moritz',description:'Some information about the beer because people want to know what they drink',price:'€1,30',image:'https://moritz.com/media/wysiwyg/MORITZ.png' },
+//     {id: 3,name:'Red Horse',description:'Some information about the beer because people want to know what they drink',price:'€2,50',image:'https://www.monde-selection.com/wp-content/uploads/2019/08/_DSF0521.png' },
+//     {id: 4,name:'Cusqueña',description:'Some information about the beer because people want to know what they drink',price:'€2,00',image:'https://jotajotafoods.com/wp-content/uploads/2020/07/CER00014.png' },
+//     {id: 5,name:'Feldschlösschen',description:'Some information about the beer because people want to know what they drink',price:'€3,00',image:'https://www.carlsberggroup.com/media/1436/fieldschlossen-weihnachtsbier.png' },
+//     {id: 6,name:'Perła ',description:'Some information about the beer because people want to know what they drink',price:'€2,60',image:'https://topdeli.co.uk/wp-content/uploads/2018/03/perla-but.png' },   
+//     {id: 7,name:'Närke Kaggen Stormaktsporter',description:'Some information about the beer because people want to know what they drink',price:'€3,20',image:'https://glasbanken.se/wp-content/uploads/Kaggen-Stormaktsporter-2019-Na%CC%88rke-Kulturbryggeri.png' },
+//     {id: 8,name:'Hite',description:'Some information about the beer because people want to know what they drink',price:'€4,00',image:'https://produits.bienmanger.com/35121-0w470h470_Hite_Lager_Beer_From_Kore.jpg' }
+// ]
 
 const Products = () => {
     const classes = useStyles();
+
+    const [ products, setProducts ] = useState([])
+    useEffect(() => {
+        getProducts()
+    }, []);
+      
+    const getProducts = async () => {
+      try {
+        const products = await axios.get( `${process.env.REACT_APP_API_BASE_URL}/products/`) 
+        // setProducts(products.data.allProducts)
+
+        // console.log(products.data.data)
+        setProducts(products.data.data)
+    //    products.data.data.map(beer => console.log(beer));
+      }
+      catch(error) {
+        console.log('error = ',error)
+      }
+  }; 
+  
     return (
         <>
         <Navbar />
