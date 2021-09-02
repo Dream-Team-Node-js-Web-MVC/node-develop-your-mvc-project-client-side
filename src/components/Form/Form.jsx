f108bc606155356d7df685bf75f6cb3e0f1ffae5
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-09-02 15:50:01 +0200

    feat: handle clear functionality added on clear button of form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,6 +17,10 @@ function Form() {
         
     }
 
+    const handleClear = () => {
+        setNewEmployee({fullName: "",email:"", password:"",role:"",profileImage:""})
+    }
+
     const postEmployee = async () => {
         try {
              const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/workers/`, newEmployee)
@@ -30,51 +34,59 @@ function Form() {
 
     const classes = useStyles();
     return (
-        <>
-        <form className={classes.root} onSubmit={handleSubmit}>
-            <Grid container>
-                <Grid item xs={12} sm={6} md={4} lg={3} >
-            <TextField className={classes.input}
-            onChange={handleChange}
-            autoComplete="off"
-             name="fullName"
-            label="Full Name"
-            variant="filled"
-             />
-              <TextField className={classes.input}
-               onChange={handleChange}
-               autoComplete="off"
-             name="email"
-            label="Email"
-            variant="filled"
-             />
-            <TextField className={classes.input}
-             onChange={handleChange}
-             autoComplete="off"
-             name="password"
-            label="Password"
-            type="password"
-            variant="filled"
-             />
-            <TextField className={classes.input}
-            select
-            onChange={handleChange}
-            defaultValue="employee"
-             name="role"
-            label="Role"
-            variant="filled"
-             >
-             <MenuItem value="admin">Admin</MenuItem>
-             <MenuItem value="employee">Employee</MenuItem>
-             </TextField>  
-                 <Button type="submit" className={classes.buttons}   variant="contained" color="primary">Submit</Button>
-                 <Button className={classes.buttons} variant="contained" color="secondary" >Clear</Button>
-             </Grid>
-             </Grid>
-           
-        </form>
-        </>
-        
+<>
+    <form className={classes.root} onSubmit={handleSubmit}>
+        <Grid container>
+            <Grid item xs={12} sm={6} md={4} lg={3} >
+                <TextField className={classes.input}
+                    required
+                    onChange={handleChange}
+                    type="text"
+                    value={newEmployee.fullName}
+                    autoComplete="off"
+                    id="input"
+                    name="fullName"
+                    label="Full Name"
+                    variant="filled"
+                />
+                <TextField className={classes.input}
+                    onChange={handleChange}
+                    required
+                    value={newEmployee.email}
+                    autoComplete="off"
+                    name="email"
+                    label="Email"
+                    variant="filled"
+                />
+                <TextField className={classes.input}
+                    onChange={handleChange}
+                    required
+                    value={newEmployee.password}
+                    autoComplete="off"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    variant="filled"
+                />
+                <TextField className={classes.input}
+                    select
+                    required
+                    value={newEmployee.role}
+                    onChange={handleChange}
+                    defaultValue="employee"
+                    name="role"
+                    label="Role"
+                    variant="filled"
+                >
+                    <MenuItem value="admin">Admin</MenuItem>
+                    <MenuItem value="employee">Employee</MenuItem>
+                </TextField>  
+                    <Button type="submit" className={classes.buttons}   variant="contained" color="primary">Submit</Button>
+                    <Button className={classes.buttons} variant="contained" color="secondary" onClick={handleClear} >Clear</Button>
+            </Grid>
+        </Grid>
+    </form>
+</>     
     )
 }
 
