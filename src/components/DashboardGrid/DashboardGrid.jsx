da87311d09146f4518a026e14e6be22283ea5d1e
Jordi <aejordi@gmail.com>
2021-09-04 19:21:46 +0200

    feat: delete function implemented - whole crud done

--- src/components/DashboardGrid/DashboardGrid.jsx
+++ src/components/DashboardGrid/DashboardGrid.jsx
@@ -7,86 +7,6 @@ import useStyles from './styles';
 
 import axios from "axios";
 
-const columns = [
-    {
-    field: 'id',
-    headerName: 'Id',
-    hide: "true",
-    width: 200,
-    editable: false,
-    },
-    {
-    field: 'fullName',
-    headerName: 'Full Name',
-    width: 200,
-    editable: true
-    },
-    {
-    field: 'email',
-    headerName: 'Email',
-    width: 200,
-    editable: true,
-    },
-    {
-    field: 'password',
-    headerName: 'Password',
-    width: 200,
-    editable: true,
-    valueFormatter: () => {
-        return "*********";
-    },
-    filterable: false,
-    sortable: false,
-    },
-    {
-    field: 'role',
-    headerName: 'Role',
-    type: 'singleSelect',
-    valueOptions: [
-        "employee",
-        "admin"
-    ],
-    width: 200,
-    editable: true,
-    },
-    {
-        field: 'actions',
-        headerName: 'Remove',
-        renderCell: IconToolbar,
-        sortable: false,
-        width: 100,
-        filterable: false,
-        align: 'center',
-        disableColumnMenu: true,
-        disableReorder: true,
-    },
-];
-
-function IconToolbar(props) {
-
-    const { id } = props;
-    
-    const remove = async () => {
-        await axios.delete(`${process.env.REACT_APP_API_BASE_URL}/workers/${id}`);
-        //const remainingWorkers = workers.filter(worker => worker._id === id);
-        //setWorkers(remainingWorkers);
-    }
-
-    return (
-        <div>
-        <IconButton
-            color="inherit"
-            size="small"
-            aria-label="delete"
-            onClick={remove}
-
-        >
-        <DeleteIcon fontSize="small" />
-        </IconButton>
-        </div>
-    )
-}
-
 function DashboardGrid() {
     const classes = useStyles();
 
@@ -118,6 +38,84 @@ function DashboardGrid() {
     }
 
     const rows = dataRows();
+
+    const columns = [
+        {
+        field: 'id',
+        headerName: 'Id',
+        hide: "true",
+        width: 200,
+        editable: false,
+        },
+        {
+        field: 'fullName',
+        headerName: 'Full Name',
+        width: 200,
+        editable: true
+        },
+        {
+        field: 'email',
+        headerName: 'Email',
+        width: 200,
+        editable: true,
+        },
+        {
+        field: 'password',
+        headerName: 'Password',
+        width: 200,
+        editable: true,
+        valueFormatter: () => {
+            return "*********";
+        },
+        filterable: false,
+        sortable: false,
+        },
+        {
+        field: 'role',
+        headerName: 'Role',
+        type: 'singleSelect',
+        valueOptions: [
+            "employee",
+            "admin"
+        ],
+        width: 200,
+        editable: true,
+        },
+        {
+            field: 'actions',
+            headerName: 'Remove',
+            renderCell: function IconToolbar(props) {
+
+                const { id } = props;
+                
+                const remove = async () => {
+                    await axios.delete(`${process.env.REACT_APP_API_BASE_URL}/workers/${id}`);
+                    const remainingWorkers = workers.filter(worker => worker._id !== id);
+                    setWorkers(remainingWorkers);
+                }
+            
+                return (
+                    <div>
+                    <IconButton
+                        color="inherit"
+                        size="small"
+                        aria-label="delete"
+                        onClick={remove}
+            
+                    >
+                    <DeleteIcon fontSize="small" />
+                    </IconButton>
+                    </div>
+                )
+            },
+            sortable: false,
+            width: 100,
+            filterable: false,
+            align: 'center',
+            disableColumnMenu: true,
+            disableReorder: true,
+        },
+    ];
 
     const handleEdit = async ({ id, field, value }) => {
         try {
